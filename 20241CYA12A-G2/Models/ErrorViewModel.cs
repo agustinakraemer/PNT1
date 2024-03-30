4558366aa423f59154f2d5a311d47e9c1af5284d
fmelo307 <fernando.melo@outlook.com.ar>
2024-03-30 16:05:52 -0300

    Init

---
+++ 20241CYA12A-G2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20241CYA12A_G2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

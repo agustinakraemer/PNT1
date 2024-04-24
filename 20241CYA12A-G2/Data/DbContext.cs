d433eb516243ae79fa0db75c8c71f21e5ec13999
anais04 <anadiaz.ar5@gmail.com>
2024-04-24 19:31:48 -0300

    Agrego Scaffold

--- 20241CYA12A-G2/Data/DbContext.cs
+++ 20241CYA12A-G2/Data/DbContext.cs
@@ -13,4 +13,24 @@ using _20241CYA12A_G2.Models;
         }
 
         public DbSet<_20241CYA12A_G2.Models.Contacto> Contacto { get; set; } = default!;
+
+        public DbSet<_20241CYA12A_G2.Models.Carrito>? Carrito { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.CarritoItem>? CarritoItem { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Categoria>? Categoria { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Cliente>? Cliente { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Descuento>? Descuento { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Empleado>? Empleado { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Pedido>? Pedido { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Producto>? Producto { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Reclamo>? Reclamo { get; set; }
+
+        public DbSet<_20241CYA12A_G2.Models.Reserva>? Reserva { get; set; }
     }
